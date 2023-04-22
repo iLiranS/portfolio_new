a8b483e18e38f1405b08080d130a075156a22b09
LiranS <lirangamerz@gmail.com>
2023-04-22 18:37:31 +0300

    added Analytic

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 ## Personal Portfolio 
-
-# Things missing : custom 404 page
-- This feature is under development in Next so not much to do :/
-- Page transitions.
+Including About me , Projects and small blog.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import SplineObject from '@/components/Spline object/SplineObject';
 import Header from '@/components/Header/Header';
+import {Analytics } from '@vercel/analytics/react'
+
 
 export default function RootLayout({
   children,
@@ -24,6 +26,7 @@ export default function RootLayout({
        <main className=' min-h-[100dvh]  transition-colors
        bg-lightBG text-darkBG
        dark:bg-darkBG dark:text-lightBG'>
+                <Analytics/>
 
         <div className=' max-w-2xl w-full relative m-auto pt-16 md:pt-24  scroll-smooth '>
           <div className='scale-75 md:scale-90 h-[250px]'><SplineObject/></div>
